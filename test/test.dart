df6b4c0cb57f814a683f7411114658471199c9b9
Yif <31947718+YitFei@users.noreply.github.com>
2024-11-12 22:25:23 +0800

    add new mtdata type  and batch actions

--- test/test.dart
+++ test/test.dart
@@ -1,34 +1,52 @@
 // ignore_for_file: avoid_print
 import 'package:mt_data_helper/src/mt_data_adapter.dart';
 import 'package:mt_data_helper/src/mt_data_source.dart';
-import 'package:mt_data_helper/src/list_extension.dart';
 import 'package:mt_data_helper/src/mt_data_row.dart';
 import 'package:mt_data_helper/src/mt_datatable.dart';
 import 'package:mt_data_helper/src/printing.dart';
 
-List<MTDataRow<Map<String, dynamic>>> mockData = [
-  MTDataRow(data: {"name": "a", "title": "1"}),
-  MTDataRow(data: {"name": "b", "title": "2"}),
-  MTDataRow(data: {"name": "c", "title": "3"}),
-  MTDataRow(data: {"name": "d", "title": "4"}),
-];
+// List<MTDataRow<Map<String, dynamic>>> mockData = [
+//   MTDataRow(data: {"name": "a", "title": "1"}),
+//   MTDataRow(data: {"name": "b", "title": "2"}),
+//   MTDataRow(data: {"name": "c", "title": "3"}),
+//   MTDataRow(data: {"name": "d", "title": "4"}),
+// ];
 void main() async {
-  MTDatatable<Map<String, dynamic>> datatable = MTDatatable();
-  datatable.load(mockData);
+  MTDatatable<MTMap> datatable = MTDatatable();
+  datatable.load([
+    MTDataRow(data: MTMap({"name": "1"}))
+  ]);
 
-  var ds = SqlDatasource<Map<String, dynamic>>(
+  var ds = SqlDatasource<MTMap>(
       MTDatasourceConfig(tableName: '', primaryKeyName: ''));
-  MTDataAdapter<Map<String, dynamic>> adapter =
-      await MTDataAdapter.initialize(ds);
+  MTDataAdapter<MTMap> adapter = await MTDataAdapter.initialize(ds);
 
   await adapter.fill(datatable);
+  datatable.newRow = MTDataRow(data: MTMap({"name": "new inserted"}));
+  datatable.rows.remove(datatable.rows.last);
+  adapter.update();
 }
 
-class SqlDatasource<T> extends MTDataSource<T> {
+class SqlDatasource<T extends MTData> extends MTDataSource<T> {
   SqlDatasource(super.config);
 
   @override
-  Future<void> initialize() async {}
+  Future<void> batchDeleteData(List<MTDataRow<T>> data) async {
+    infoPrint("deleted");
+    warningPrint(data.map((e) => e.data.toMap()['name']).toString());
+  }
+
+  @override
+  Future<void> batchInsertData(List<MTDataRow<T>> data) async {
+    infoPrint("inserted");
+    warningPrint(data.map((e) => e.data.toMap()['name']).toString());
+  }
+
+  @override
+  Future<void> batchUpdateData(List<MTDataRow<T>> data) async {
+    infoPrint("updated");
+    warningPrint(data.map((e) => e.data.toMap()['name']).toString());
+  }
 
   @override
   Future<List<T>> fetchData() async {
@@ -36,20 +54,26 @@ class SqlDatasource<T> extends MTDataSource<T> {
   }
 
   @override
-  Future<void> insertData(T data) async {}
-
-  @override
-  Future<void> updateData(T data) async {}
-
-  @override
-  Future<void> deleteData(T data) async {}
+  Future<void> initialize() async {}
 }
 
-class Test implements DeepCopyable<Test> {
+class Test implements MTData {
   String name;
   Test({
     required this.name,
   });
+
+  factory Test.fromMap(Map<String, dynamic> map) {
+    return Test(name: map['name']);
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {"name": name};
+  }
+
+  @override
+  MTData fromMap(Map<String, dynamic> map) => fromMap(map);
 
   @override
   Test copyWith({
